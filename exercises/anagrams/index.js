416cc8ce3ba3cf2e2ab4df889802ebed8a3e5c01
ebrault <emmet.brault@gmail.com>
2019-03-04 15:55:47 -0500

    Added responses
    Added responses for anagrams, capitalize, chunk, and steps

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -8,6 +8,62 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB) {}
+function anagrams(stringA, stringB) {
+  return sanitize(stringA) === sanitize(stringB);
+}
+
+function sanitize(str) {
+  return str.replace(/[^\w]/g, '').toLowerCase().split('').sort().join('');
+}
 
 module.exports = anagrams;
+
+// Solution 1:
+// function(stringA, stringB) {
+  // stringA = stringA.replace(/[^\w]/g, '').toLowerCase();
+  // stringB = stringB.replace(/[^\w]/g,'').toLowerCase();
+  // let charsA = {};
+  // let charsB = {};
+  // for (let char of stringA) {
+  //   charsA[char] = charsA[char] + 1 || 1;
+  // }
+  // for (let char of stringB) {
+  //   charsB[char] = charsB[char] + 1 || 1;
+  // }
+  // if (stringA.length !== stringB.length) {
+  //   return false;
+  // } else {
+  //   for (let char in charsA) {
+  //     if (charsA[char] !== charsB[char]) {
+  //       return false;
+  //     }
+  //   }
+  // }
+  // return true;
+// }
+
+// Solution 2:
+// function(stringA, stringB) {
+//   let charMapA = buildCharMap(stringA);
+//   let charMapB = buildCharMap(stringB);
+//
+//   if (Object.keys(charMapA).length !== Object.keys(charMapB).length) {
+//     return false;
+//   }
+//   for (let char in charMapA) {
+//     if (charMapA[char] !== charMapB[char]) {
+//       return false;
+//     }
+//   }
+//   return true;
+//   }
+// }
+//
+//
+// function buildCharMap(str) {
+//   let charMap = {};
+//   for (let char of str.replace(/[^\w]/g, '').toLowerCase()) {
+//     charMap[char] = charMap[char] + 1 || 1;
+//   }
+//   return charMap;
+// }
